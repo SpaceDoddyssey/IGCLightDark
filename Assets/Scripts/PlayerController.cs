a41884879bd7e922da7bd1d30c40e567cf302eb1
SpaceDoddyssey <cameronradodd@gmail.com>
2023-04-17 23:43:03 -0700

    Movement!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool smoothTransition = true;
+    public float transitionSpeed = 10f;
+    public float transitionRotationSpeed = 500f;
+
+    //Private variables
+    Vector3 targetGridPos, prevTargetGridPos, targetRotation;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        targetGridPos = Vector3Int.RoundToInt(transform.position);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        HandleMovement();
+    }
+
+    void HandleMovement(){
+        prevTargetGridPos = targetGridPos;
+        Vector3 targetPosition = targetGridPos;
+    
+        //Keep the rotation values in bound
+        if(targetRotation.y > 270f && targetRotation.y < 361f) targetRotation.y = 0f;
+        if(targetRotation.y < 0f) targetRotation.y = 270f;
+
+        if(!smoothTransition){
+            transform.position = targetPosition;
+            transform.rotation = Quaternion.Euler(targetRotation);
+        } else {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * transitionSpeed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), Time.deltaTime * transitionRotationSpeed);
+        }
+    }
+
+    private float scalar = 2; //Right now the grid is such that the player should move 2 units. That may change.
+    public void MoveForward(){
+        if (AtRest) { targetGridPos += transform.forward * scalar; }
+    }
+    public void MoveBackward(){
+        if (AtRest) { targetGridPos -= transform.forward * scalar; }
+    }
+    public void MoveLeft(){
+        if (AtRest) { targetGridPos -= transform.right * scalar; }
+    }
+    public void MoveRight(){
+        if (AtRest) { targetGridPos += transform.right * scalar; }
+    }
+    public void RotateLeft(){ 
+        if (AtRest) { targetRotation -= Vector3.up * 90f; }
+    }
+    public void RotateRight(){ 
+        if (AtRest) { targetRotation += Vector3.up * 90f; }
+    }
+
+    bool AtRest {
+        get {
+            //Note to future self: Worry that allowing some variance here may cause the player to get slightly misaligned with the grid.
+            //If that becomes an issue, start here to find a fix.
+            if((Vector3.Distance(transform.position, targetGridPos) < 0.05f) &&
+               (Vector3.Distance(transform.eulerAngles, targetRotation) < 0.05f)) {
+                return true;
+               } else { 
+                return false;
+            }
+        }
     }
 }
