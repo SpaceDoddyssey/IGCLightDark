1643ca26221e382c167f53e42f561f20f017208b
SpaceDoddyssey <cameronradodd@gmail.com>
2023-07-28 19:46:08 -0700

    Added potion and fireball items

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,25 +4,48 @@ using UnityEngine;
 
 public enum ItemType
 {
-    None,
     Potion, 
-    FireballScroll
+    FireballScroll,
+    None //If you add more always put None last
 }
 
-public class Item : MonoBehaviour
+public static class Item
 {
-    public Sprite itemSprite;
-    public GameObject itemPrefab;
-    public ItemType itemType;
+    // static Sprite[] itemSprites;
+
+    // static Item(){
+    //     var potionSprite = Resources.Load("ph_potion") as Sprite;
+    //     var fireballSprite = Resources.Load("ph_fireball_scroll") as Sprite;
+    //     itemSprites = new Sprite[] { potionSprite, fireballSprite };
+    // }
+
+    //public static (ItemType, Sprite) RandomItemData(){
+    public static ItemType RandomItemData(){
+        int type = Random.Range(0, (int)ItemType.None); //Update this number if you add more item types
+        //return ((ItemType)type, itemSprites[type]); 
+        return (ItemType)type;
+    }
 
     public static bool Use(ItemType t){
         switch(t){
             case ItemType.Potion: 
                 return UsePotion();
+            case ItemType.FireballScroll:
+                return UseFireball();
             default: 
-                print("Default!"); 
+                Debug.Log("Default!"); 
                 return false;
         }
+    }
+
+    public static bool UseFireball(){
+        GameObject player = GameObject.Find("Player");
+        GameObject fballPrefab = Resources.Load("Fireball", typeof(GameObject)) as GameObject;
+        GameObject fball = Object.Instantiate(fballPrefab, player.transform.position, Quaternion.identity);
+        FireballScript fScript= fball.GetComponent<FireballScript>();
+        Debug.Log("Item script says: " + player.transform.forward);
+        fScript.dir = player.transform.forward;
+        return true;        
     }
 
     public static bool UsePotion(){
@@ -30,11 +53,10 @@ public class Item : MonoBehaviour
 
         GameState stateScript = GameObject.Find("Game World Manager").GetComponent<GameState>();
 
-        
-
         //The potion brings you up to "healAmount" closer to the center of the health bar
         int damage = System.Math.Abs(stateScript.playerHealth);
         if(damage == 0){
+            //Does not use up the item if you're already at 0 damage
             Debug.Log("No damage to heal!");
             return false; 
         }
